ac3f27441480343c3f000b0d6e6e69b1565f2539
Diego Lopez <Lopezdiego4167@hotmail.com>
2024-06-22 17:00:14 -0300

    asd

---
+++ expenset_tracker.py
@@ -0,0 +1,18 @@
+
+def main():
+    print(f"Running Expense Tracker")
+    
+
+    # Get user to input their expense
+    
+
+
+    # write it to a file
+
+
+
+    # read said file and summerize expenses
+    pass
+
+if __name__ == "__main__":
+    main()
